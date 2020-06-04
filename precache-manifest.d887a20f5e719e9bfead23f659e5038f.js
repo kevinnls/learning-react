5d73e5ff9769799fd7ba6d53a2827c1e926b911d
kevinnls <kevin.nl.samuel@gmail.com>
2020-06-04 23:27:22 +0530

    Updates

---
+++ precache-manifest.d887a20f5e719e9bfead23f659e5038f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d743863fe9de36dc2032e87db350e4d0",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "bd83a344e2f0753a0688",
+    "url": "/learning-react/static/css/main.4afa2deb.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/learning-react/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-react/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd83a344e2f0753a0688",
+    "url": "/learning-react/static/js/main.ef9be191.chunk.js"
+  },
+  {
+    "revision": "560d333713d8ddce6c1d",
+    "url": "/learning-react/static/js/runtime-main.50aeef1a.js"
+  }
+]);
