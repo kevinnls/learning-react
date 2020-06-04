ad2e84901f352c8e0ae265f1b9ee6c64d11b62c7
kevinnls <kevin.nl.samuel@gmail.com>
2020-06-04 22:53:05 +0530

    config package.json and build

--- my-app/build/service-worker.js
+++ my-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.002fb3fe681a0d8a2aa8c54ebb5c474f.js"
+  "/learning-react/precache-manifest.d887a20f5e719e9bfead23f659e5038f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learning-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
