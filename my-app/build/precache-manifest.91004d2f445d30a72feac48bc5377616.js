1e901ab7e438514ccbe53296a0c5b29342a52582
kevinnls <kevin.nl.samuel@gmail.com>
2020-06-04 17:32:57 +0530

    built for deployment to gae

---
+++ my-app/build/precache-manifest.91004d2f445d30a72feac48bc5377616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfe2c0709b2809551be4a01a203ee6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd83a344e2f0753a0688",
+    "url": "/static/css/main.4afa2deb.chunk.css"
+  },
+  {
+    "revision": "210717bcd6136923a012",
+    "url": "/static/js/2.5ccea08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ccea08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd83a344e2f0753a0688",
+    "url": "/static/js/main.ef9be191.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
