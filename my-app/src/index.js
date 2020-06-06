ff3f32c7dc3cef318efcee63c1784276fc64a579
kevinnls <kevin.nl.samuel@gmail.com>
2020-06-06 15:25:25 +0530

    modifiy package.json to include dev script

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -46,16 +46,17 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+  // constructor(props) {
+  //  super(props)
+    // this.state = {
+      state={
       history: [{
         squares: Array(9).fill(null),
         isNext: 'X',
       }],
       stepNumber: 0,
     }
-  }
+  // }
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
